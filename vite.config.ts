18b42ac1e28a6f6a0c668f509a2327ccd37a991c
silvervat <silver.vatsel@gmail.com>
2025-10-05 16:17:58 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// NB! kui repo nimi on "assembly-exporter", jäta base selliseks.
-// Kui kolid custom domeenile või Pages root'i, vaheta base: "/".
 export default defineConfig({
   plugins: [react()],
   base: "/assembly-exporter/",
-  build: { outDir: "dist" },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
 });
