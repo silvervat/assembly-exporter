5b9f118d5f3e14bb488b9ddd9eafa39c51c4f9f9
silvervat <silver.vatsel@gmail.com>
2025-10-05 15:14:01 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import * as WorkspaceAPI from "trimble-connect-workspace-api"
+import AssemblyExporter from './components/AssemblyExporter'
+import '@trimbleinc/modus-bootstrap/dist/modus.min.css'
+import '@trimble-oss/modus-icons/dist/modus-outlined/fonts/modus-icons.css'
+import './App.css'
+
+function App() {
+  const [tcApi, setTcApi] = useState<WorkspaceAPI.WorkspaceAPI>()
+
+  useEffect(() => {
+    async function connectWithTcAPI() {
+      const api = await WorkspaceAPI.connect(
+        window.parent, 
+        (_event: any, _data: any) => {}
+      );
+      setTcApi(api);
+    }
+    connectWithTcAPI();
+  }, []);
+
+  return (
+    <div className="app-container">
+      <h2 className="title">Assembly Exporter</h2>
+      {tcApi && <AssemblyExporter api={tcApi} />}
+    </div>
+  )
+}
+
+export default App
