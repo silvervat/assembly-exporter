7be69bedfa3c2e9410dffbc1536074778381df50
silvervat <silver.vatsel@gmail.com>
2025-10-05 16:13:44 +0300

    Refactor import statements and connect function

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import { useEffect, useState } from 'react'
-import * as WorkspaceAPI from "trimble-connect-workspace-api"
-import AssemblyExporter from './components/AssemblyExporter'
-import '@trimbleinc/modus-bootstrap/dist/modus.min.css'
-import '@trimble-oss/modus-icons/dist/modus-outlined/fonts/modus-icons.css'
-import './App.css'
+import { useEffect, useState } from "react";
+import * as WorkspaceAPI from "trimble-connect-workspace-api";
+import AssemblyExporter from "./components/AssemblyExporter";
+import "@trimbleinc/modus-bootstrap/dist/modus.min.css";
+import "@trimble-oss/modus-icons/dist/modus-outlined/fonts/modus-icons.css";
+import "./App.css";
 
 function App() {
-  const [tcApi, setTcApi] = useState<WorkspaceAPI.WorkspaceAPI>()
+  const [tcApi, setTcApi] = useState<WorkspaceAPI.WorkspaceAPI>();
 
   useEffect(() => {
     async function connectWithTcAPI() {
-      const api = await WorkspaceAPI.connect(
-        window.parent, 
-        (_event: any, _data: any) => {}
-      );
+      const api = await WorkspaceAPI.connect(window.parent, () => {});
       setTcApi(api);
     }
     connectWithTcAPI();
@@ -24,7 +21,7 @@ function App() {
       <h2 className="title">Assembly Exporter</h2>
       {tcApi && <AssemblyExporter api={tcApi} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
